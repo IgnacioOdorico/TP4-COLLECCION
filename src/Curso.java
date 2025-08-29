0a5063bb382c6f8fe914b613e1781d5556c38976
IgnacioOdorico <cony20082008@gmail.com>
2025-08-29 10:28:10 -0300

    Subiendo cambios

--- src/Curso.java
+++ src/Curso.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class Curso {
+    private String nombre;
+    private String docente;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocente() {
+        return docente;
+    }
+
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
+
+
+    public Curso(String nombre, String docente) {
+        this.nombre = nombre;
+        this.docente = docente;
+    }
+
+    public Curso() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "nombre='" + nombre + '\'' +
+                ", docente='" + docente + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Curso curso)) return false;
+        return Objects.equals(nombre, curso.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nombre);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Map < String, Curso> cursos = new HashMap<>();
+
+        Curso c1= new Curso("prog3","matias");
+        Curso c2= new Curso("matematicas","sebastian");
+        Curso c3= new Curso("base de datos","jorge");
+        Curso c4= new Curso("prog3","marquitos");
+
+        cursos.put("MATERIA1",c1);
+        cursos.put("MATERIA2",c2);
+        cursos.put("MATERIA3",c3);
+        //NO SE AGREGA
+        cursos.put("MATERIA1",c4);
+        //SI SE AGREGA
+        cursos.put("MATERIA4",c4);
+
+
+
+        System.out.println("Iterando con entrySet():");
+        for (Map.Entry<String,Curso> entry : cursos.entrySet()) {
+            String curso = entry.getKey();
+            Curso profe = entry.getValue();
+            System.out.println(curso + " tiene " + profe);
         }
+
+        //NOTA
+        //EN MAP SE REMPLASAN LOS VALORES SI TIENE LA MISMA KEY
+
     }
 }
